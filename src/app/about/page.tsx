388788cd65c87780acb3873b7276917af457f846
Arief Kurniawan <78076941+arief-github@users.noreply.github.com>
2025-06-15 10:18:30 +0700

    init layout

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <main>
+            <h1 className='text-4xl font-bold'>About Us</h1>
+            <div className='mt-5'>
+                About us and Our awesome application
+            </div>
+        </main>
+    );
+};
+
+export default AboutPage;
