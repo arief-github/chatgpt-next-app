63c06ba695df01f9ae87aca0eef4372f424ec832
Arief Kurniawan <78076941+arief-github@users.noreply.github.com>
2025-07-12 14:46:13 +0700

    remove client component

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import ServerComponent from "@/ServerComponent";
-import ClientComponent from "@/ClientComponent";
 const HomePage = async () => {
-    const req = await fetch("https://reqres.in/api/users/2")
-    const { data } = await req.json()
-
     return (
         <main className='mt-5 max-w-xl mx-auto'>
             <ServerComponent/>
-            <ClientComponent name={`${data.first_name} ${data.last_name}`} />
         </main>
     );
 };
